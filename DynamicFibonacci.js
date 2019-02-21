c5c7e1bff01d2ee02f3091a944036edc6e4fa559
zbs881314 <34663445+zbs881314@users.noreply.github.com>
2019-02-20 21:34:33 -0500

    Add files via upload

---
+++ DynamicFibonacci.js
@@ -0,0 +1,41 @@
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233...
+
+let calculations = 0;
+function fibonacci(n) {
+  calculations++;
+  if (n<2) {
+    return n
+  }
+  return fibonacci(n-1) + fibonacci(n-2);
+}
+
+function fibonacciMaster() {
+  let cache = {};
+  return function fib(n) {
+    calculations++;
+    if (n in cache) {
+      return cache[n];
+    } else {
+      if (n<2) {
+        return n;
+      } else {
+        cache[n] = fib(n-1) + fib(n-2);
+        return cache[n];
+      }
+    }
+  }
+}
+
+function fibonacciMaster2(n) {
+  let answer = [0, 1];
+  for (let i = 2; i <= n; i++) {
+    answer.push(answer[i-2] + answer[i-1]);
+  }
+  return answer.pop();
+}
+
+const fasterFib = fibonacciMaster();
+console.log('Slow', fibonacci(20))
+console.log('Dp', fasterFib(100));
+console.log('Dp2', fibonacciMaster2(100));
+console.log('we did ' + calculations + ' calculations');
