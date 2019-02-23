74f5e784cff8a5b540941fafe6eb128ac8b93527
zbs881314 <34663445+zbs881314@users.noreply.github.com>
2019-02-22 20:40:43 -0500

    Add files via upload

---
+++ ExcelSheetColumnNumber.java
@@ -0,0 +1,7 @@
+class Solution {
+  public int titleToNumber(String s) {
+    int result = 0;
+    for (int i = 0; i<s.length(); result = result * 26 + (s.charAt(i) - 'A' + 1), i++);
+    return result;
+  }
+}
